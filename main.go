e6d3d135ab493d38888dfbcc2ce7def36dd1b71a
Stanley <stanleyye1996@gmail.com>
2017-05-20 22:04:37 -0700

    Added User model

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"gopkg.in/maciekmm/messenger-platform-go-sdk.v4"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type campaignNode struct {}
+type campaignNode struct{}
 
+// TODO: Combine these vars into a group?
 var cbMessenger = &messenger.Messenger{
 	AccessToken: os.Getenv("TOKEN"),
 }
+
+// Initialize mongoDB session
+var mongoSession, mongoSessionErr = mgo.Dial("<INSERT MONGO DB INSTANCE URL HERE>")
 
 func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
 	profile, err := cbMessenger.GetProfile(opts.Sender.ID)
@@ -25,49 +31,66 @@ func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg mess
 		return
 	}
 
-	// send a simple message
-	resp, err := cbMessenger.SendSimpleMessage(
-		opts.Sender.ID,
-		fmt.Sprintf("Hello, %s %s, %s", profile.FirstName, profile.LastName, msg.Text),
-	)
+	if toLower(msg) == "start" {
+		startTestCampaign()
+		return
+	} else if len(msg) > 0 {
+		// chatbot only understands the message "start", any other message that is not a button or "start"
+		// is invalid
+		resp, err := cbMessenger.SendSimpleMessage(
+			opts.Sender.ID,
+			fmt.Sprintf("Sorry, I don't understand your message."),
+		)
 
-	if err != nil {
-		fmt.Println(err)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("%+v", resp)
 	}
 
-	fmt.Printf("%+v", resp)
 }
 
 // Handles incoming requests for the /campaign endpoint
 func campaignHandler(rw http.ResponseWriter, req *http.Request) {
-	file, err := ioutil.ReadFile("./campaign-node.json")
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
 	// TODO: query through every user in the database and send the campaign to everyone
 	// TODO: have a table in the database to keep track of which campaign (by ID) were sent out
 	// so that the same campaign doesn't get sent out multiple times. If an editor needs to edit
 	// a campaign, the Mgmt-Api should create a new ID. Still need to think this through. What if
-	// an editor needs to change a campaign while a user is in the middle of a campaign? 
+	// an editor needs to change a campaign while a user is in the middle of a campaign?
 	// TODO: ask at next meeting if its necessary to hash the details of users?
 
+	fmt.Fprintf(rw, "<h1>Campaign</h1>")
+}
 
-	fmt.Fprintf(rw, "<h1>Testing 123</h1><div>hey/div>")
+// Test campaign
+func startTestCampaign() {
+	raw, err := ioutil.ReadFile("./camfpaign-node.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
 
 func main() {
+	// panic if mongoDB session fails initialization
+	if mongoSessionErr != nil {
+		panic(mongoSessionErr)
+	}
+	defer mongoSession.close()
+
 	// TODO: remove the TOKEN variable in production. It doesn't matter if the token is seen
-	// since it is only a test cb page. This is also only used locally on your computer, 
-	// Heroku supports environment variables. 
+	// since it is only a test cb page. This is also only used locally on your computer,
+	// Heroku supports environment variables.
 	apiToken := "EAATZAxfQTVYQBAE08rAVR3NZBaPwe0FTDEbGZBLbIKx5LUf5Y5m2DiZAkg1ZBhxo0IKhGuLHMkMj3ZAXdOJygZBTK9KZCyGb8J87uxsxpFXZCrzZByLveD2cmHryuCxDNtv2ifVlM18J1QoktcHLDaJI59Vlvn120t613QrQ2Ae0GSnwZDZD"
-	os.SetEnv("TOKEN", apiToken)
+	os.Setenv("TOKEN", apiToken)
 
 	cbMessenger.MessageReceived = MessageReceived
 
+	// API endpoints
 	http.HandleFunc("/webhook", cbMessenger.Handler)
 	http.HandleFunc("/campaign", campaignHandler)
 
+	// TODO: Set it to Heroku's env PORT || 3001 when deplying to Heroku
 	log.Fatal(http.ListenAndServe(":3001", nil))
 }

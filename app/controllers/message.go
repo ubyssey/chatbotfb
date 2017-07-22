2b635c8e5216bc9b306d7873c83f010715972eb3
Stanley <stanleyye1996@gmail.com>
2017-07-21 23:15:04 -0700

    Added an option to display a menu when 'menu' is called

--- app/controllers/message.go
+++ app/controllers/message.go
@@ -19,10 +19,12 @@ import (
 )
 
 var (
-	dbName   string
-	uc       *mgo.Collection // User Collection
-	ucError  error           // User Collection error
-	senderID string
+	campaignCollection  *mgo.Collection
+	dbName              string
+	userCollection      *mgo.Collection
+	userCollectionError error
+	senderID            string
+	startCampaignId     = "6z479nb9-3x2f-23gs-g2dz-abc10625xc68"
 )
 
 // Creates a user record in MongoDB if non-existing user, otherwise
@@ -31,7 +33,7 @@ func createOrUpdateUser(opts messenger.MessageOpts) {
 	// Check whether a user exists or not. If they are a first time user, create a record in database
 	// otherwise update the record of that user
 	// TODO: figure out when lastMessage is updated since there are two user actions
-	if ucError == nil {
+	if userCollectionError == nil {
 		// existing user (user is found)
 		set := bson.M{
 			"lastSeen": time.Unix(opts.Timestamp, 0),
@@ -45,12 +47,12 @@ func createOrUpdateUser(opts messenger.MessageOpts) {
 			},
 		}
 
-		uc.UpdateId(senderID, bson.M{"$set": set})
+		userCollection.UpdateId(senderID, bson.M{"$set": set})
 
 		printlogger.Log("Updated User %s", senderID)
 	} else {
 		// create new user
-		uc.Insert(
+		userCollection.Insert(
 			&user.User{
 				senderID,
 				time.Unix(opts.Timestamp, 0),
@@ -69,88 +71,102 @@ func createOrUpdateUser(opts messenger.MessageOpts) {
 	}
 }
 
+func defaultMessage() {
+	resp, msgErr := chatbot.CbMessenger.SendSimpleMessage(
+		opts.Sender.ID,
+		fmt.Sprintf("Sorry, I don't understand your message."),
+	)
+
+	if msgErr != nil {
+		printlogger.Log(msgErr.Error())
+	}
+	printlogger.Log("%+v", resp)
+}
+
 // Sends a reply back to the user depending on their message content
 func handleReplyMessage(opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
-	campaignCollection := database.MongoSession.DB(dbName).C("campaigns")
+	messageText := strings.ToLower(msg.Text)
 
-	if strings.ToLower(msg.Text) == "start" {
-		// A new starting campaign for the user
-		startCampaign := campaign.Campaign{}
-		startCampaignMissingErr := campaignCollection.FindId("6z479nb9-3x2f-23gs-g2dz-abc10625xc68").One(&startCampaign)
-
-		// TODO: handle err
-		if startCampaignMissingErr != nil {
-			startCampaignErrResponse, _ := chatbot.CbMessenger.SendSimpleMessage(
-				opts.Sender.ID,
-				fmt.Sprintf("A start campaign was not found."),
-			)
-
-			printlogger.Log("%+v", startCampaignErrResponse)
-			return
-		}
-
-		// Get the button templates to be shown to the user
-		buttonsOptions, buttonOptionsErr := chatbot.GetButtonTemplateOptions(
-			startCampaign.UUID,
-			startCampaign.Nodes[startCampaign.RootNode].UserActions,
-		)
-
-		if buttonOptionsErr != nil {
-			printlogger.Log(buttonOptionsErr.Error())
-			return
-		}
-
-		// Initialize a message query
-		mq := messenger.MessageQuery{}
-		mq.RecipientID(senderID)
-
-		mq.Template(
-			template.GenericTemplate{
-				Title:   startCampaign.Name,
-				Buttons: buttonsOptions,
-			},
-		)
-
-		resp, msgErr := chatbot.CbMessenger.SendMessage(mq)
-		if msgErr != nil {
-			printlogger.Log(msgErr.Error())
-		}
-		printlogger.Log("%+v", resp)
-	} else if len(msg.Text) > 0 {
-		// chatbot only understands the message "start", any other message that is not a button or "start"
-		// is invalid
-		resp, msgErr := chatbot.CbMessenger.SendSimpleMessage(
-			opts.Sender.ID,
-			fmt.Sprintf("Sorry, I don't understand your message."),
-		)
-
-		if msgErr != nil {
-			printlogger.Log(msgErr.Error())
-		}
-		printlogger.Log("%+v", resp)
+	switch {
+	case messageText == "menu":
+		showMenu()
+	case messageText == "start":
+		startCampaign()
+	default:
+		defaultMessage()
 	}
+}
+
+func initConfigVariables() {
+	dbName = configuration.Config.Database.MongoDB.Name
+	campaignCollection = database.MongoSession.DB(dbName).C("campaigns")
+	userCollection = database.MongoSession.DB(dbName).C("users")
+	// Check to see if the sender exists in the user collection
+	userCollectionError = userCollection.FindId(senderID)
 }
 
 // Handles the POST message request from facebook
 func MessageReceived(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
-	printlogger.Log("Received message from %s", opts.Sender.ID)
+	senderID = opts.Sender.ID
+	printlogger.Log("Received message from %s", senderID)
 
 	// fetches the sender profile from facebook's Graph API
-	_, profileErr := chatbot.CbMessenger.GetProfile(opts.Sender.ID)
+	_, profileErr := chatbot.CbMessenger.GetProfile(senderID)
 	// if the sender profile is invalid, print out error and return
 	if profileErr != nil {
 		printlogger.Log(profileErr.Error())
 		return
 	}
 
-	senderID = opts.Sender.ID
-	dbName = configuration.Config.Database.MongoDB.Name
-
-	// User collection (for Mon
-	uc = database.MongoSession.DB(dbName).C("users")
-	currUser := user.User{}
-	ucError = uc.FindId(senderID).One(&currUser)
-
+	initConfigVariables()
 	createOrUpdateUser(opts)
 	handleReplyMessage(opts, msg)
+}
+
+func showMenu() {
+
+}
+
+// Start a new campaign for the user
+func startCampaign() {
+	startCampaign := campaign.Campaign{}
+	startCampaignMissingErr := campaignCollection.FindId(startCampaignId).One(&startCampaign)
+
+	if startCampaignMissingErr != nil {
+		startCampaignErrResponse, _ := chatbot.CbMessenger.SendSimpleMessage(
+			senderID,
+			fmt.Sprintf("A start campaign was not found."),
+		)
+
+		printlogger.Log("%+v", startCampaignErrResponse)
+		return
+	}
+
+	// Get the button templates to be shown to the user
+	buttonsOptions, buttonOptionsErr := chatbot.GetButtonTemplateOptions(
+		startCampaign.UUID,
+		startCampaign.Nodes[startCampaign.RootNode].UserActions,
+	)
+
+	if buttonOptionsErr != nil {
+		printlogger.Log(buttonOptionsErr.Error())
+		return
+	}
+
+	// Initialize a message query
+	mq := messenger.MessageQuery{}
+	mq.RecipientID(senderID)
+
+	mq.Template(
+		template.GenericTemplate{
+			Title:   startCampaign.Name,
+			Buttons: buttonsOptions,
+		},
+	)
+
+	resp, msgErr := chatbot.CbMessenger.SendMessage(mq)
+	if msgErr != nil {
+		printlogger.Log(msgErr.Error())
+	}
+	printlogger.Log("%+v", resp)
 }
